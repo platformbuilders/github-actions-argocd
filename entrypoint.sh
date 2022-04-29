8e458c244f37a5ae58dd1916ad0bcbc036321e7a
Renato Ruis <renatoruis@pop-os.localdomain>
2022-04-29 13:47:28 -0300

    Testando...

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,9 @@ export APPID=$1
 export USERNAME=$2
 export PASSWORD=$3
 export ARGOURL="argocd-server.argocd.svc.cluste.local"
+
+printf "\033[0;32m============> Login ArgoCD URL: $ARGOURL \033[0m\n"
 argocd login --insecure --username $USERNAME --password $PASSWORD $ARGOURL
+
 
 argocd app list
