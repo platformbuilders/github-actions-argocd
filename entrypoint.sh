58a804044cd4b3ddc6142efea7168901e2c1311e
Renato Ruis <renatoruis@pop-os.localdomain>
2022-04-29 13:55:02 -0300

    Testando...

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,23 @@ argocd version --short
 export APPID=$1
 export USERNAME=$2
 export PASSWORD=$3
-export ARGOURL="argocd-server.argocd.svc.cluste.local"
+export ARGOURL="argocd-server.argocd.svc.cluster.local"
+export APPENV=$4
 
-printf "\033[0;32m============> Login ArgoCD URL: $ARGOURL \033[0m\n"
+printf "\033[0;32m=====> Login ArgoCD URL: $ARGOURL \033[0m\n"
 argocd login --insecure --username $USERNAME --password $PASSWORD $ARGOURL
 
 
-argocd app list
+printf "\033[0;32m=====> Verify $APPID-$APPENV \033[0m\n"
+argocd app get $APPID-$APPENV
+
+
+printf "\033[0;32m=====> Sync $APPID-$APPENV \033[0m\n"
+argocd app sync $APPID-$APPENV
+
+
+if [ $? -eq 0 ]; then
+  printf "\033[0;32m=====> Sync $APPID-$APPENV Success \033[0m\n"
+else
+  printf "\033[0;31m=====> Sync $APPID-$APPENV Failed \033[0m\n"
+fi
